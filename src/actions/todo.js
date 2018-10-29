d75e3671bc4d2ba2692ba86f36122fd58767cd99
Ewan Bauld <ewan.bauld@gmail.com>
2018-10-29 19:01:08 +0000

    Add todo actions

---
+++ src/actions/todo.js
@@ -0,0 +1,9 @@
+export const addTodo = (todo) => ({
+    type: 'ADD_TODO',
+    todo
+});
+
+export const setTodos = (todos) => ({
+    type: 'SET_TODOS',
+    todos
+})
--- src/tests/actions/actions.spec.js
+++ src/tests/actions/actions.spec.js
@@ -1,19 +1,19 @@
 import { addTodo, setTodos} from '../../actions/todo.js';
-import todos from '../fixtures/todo';
+import todos from '../fixtures/todos';
 
 describe('Todo Action object', () => {
     it('should setup the set todo object', () => {
-        const action = setTodos(todos[1]);
+        const action = setTodos(todos);
         expect(action).toEqual({
             type: 'SET_TODOS',
-            todo: todos[1]
+            todos
         })
     });
     it('should setup the add todo object', () => {
-        const action = addTodo(todos);
+        const action = addTodo(todos[1]);
         expect(action).toEqual({
             type: 'ADD_TODO',
-            todos
+            todo: todos[1]
         })
     });
 
